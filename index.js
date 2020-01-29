6cf03a1fe04634e4a891d9d8e0348609adf32489
Diego Capassi <diego.capassi.moreira@gmail.com>
2020-01-28 22:42:19 -0200

    Created CRUD routes

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
 // Query Params = ?teste=1
-// Route Params = /users/1
+// Route Params = /users/1 ex:   const { name } = req.query;
 // Request Body = { name: "Diego Capassi", email: "diego.capassi.moreira@gmail.com"}
 
 const express = require("express");
 const server = express();
+server.use(express.json());
 
-//localhost:3010/route/1
-server.get("/route/:id", (req, res) => {
-  const nome = req.query.nome;
-  const id = req.params.id;
-  return res.json({ message: `Hello ${nome} id=${id}` });
+let users = [];
+
+//CRUD
+/*
+Create
+Read
+Updated
+Delete
+*/
+
+//localhost:3010/users/1
+server.get("/user/:id", (req, res) => {
+  const { id } = req.params;
+  return res.json({ user: users[id] });
+});
+
+server.get("/user/", (req, res) => {
+  return res.json(users);
+});
+
+server.post("/user/", (req, res) => {
+  const { user } = req.body;
+  if (users.indexOf(user) == -1) {
+    users.push(user);
+  }
+  return res.json(user);
+});
+
+server.put("/user/:id", (req, res) => {
+  const { user } = req.body;
+  const { id } = req.params;
+  const indexOfUser = users.indexOf(user);
+
+  if (indexOfUser == -1) {
+    users[id] = user;
+  }
+
+  return res.json(user);
+});
+
+server.delete("/user/", (req, res) => {
+  const { user } = req.body;
+  const indexOfUser = users.indexOf(user);
+
+  if (indexOfUser != -1) {
+    users.splice(indexOfUser, 1);
+  }
+  return res.json(user);
 });
 
 server.listen(3010);
