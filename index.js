ecc23d7b0c0e74a0e38ff9a4c0a5268d2deea580
Diego Capassi <diego.capassi.moreira@gmail.com>
2020-01-28 23:14:44 -0200

    Created middleware to log requests and for validation

--- index.js
+++ index.js
@@ -16,8 +16,31 @@ Updated
 Delete
 */
 
-//localhost:3010/users/1
-server.get("/user/:id", (req, res) => {
+//Middleware to log HTTP Requests and execution time
+server.use((req, res, next) => {
+  console.time("Request");
+  console.log(`Método ${req.method} URL: ${req.url}`);
+  next();
+  console.timeEnd("Request");
+});
+
+//Middleware to check if the body format is valid
+function checkBodyFormat(req, res, next) {
+  if (!req.body.user) {
+    return res.status(400).json({ error: "name field is required" });
+  }
+  next();
+}
+
+//Middleware to check if the user id params exists
+function checkIndexFormat(req, res, next) {
+  if (!users[req.params.id]) {
+    return res.status(400).json({ error: "User does not exist" });
+  }
+  next();
+}
+
+server.get("/user/:id", checkIndexFormat, (req, res) => {
   const { id } = req.params;
   return res.json({ user: users[id] });
 });
@@ -26,7 +49,7 @@ server.get("/user/", (req, res) => {
   return res.json(users);
 });
 
-server.post("/user/", (req, res) => {
+server.post("/user/", checkBodyFormat, (req, res) => {
   const { user } = req.body;
   if (users.indexOf(user) == -1) {
     users.push(user);
@@ -34,7 +57,7 @@ server.post("/user/", (req, res) => {
   return res.json(user);
 });
 
-server.put("/user/:id", (req, res) => {
+server.put("/user/:id", checkIndexFormat, checkBodyFormat, (req, res) => {
   const { user } = req.body;
   const { id } = req.params;
   const indexOfUser = users.indexOf(user);
@@ -46,14 +69,10 @@ server.put("/user/:id", (req, res) => {
   return res.json(user);
 });
 
-server.delete("/user/", (req, res) => {
-  const { user } = req.body;
-  const indexOfUser = users.indexOf(user);
-
-  if (indexOfUser != -1) {
-    users.splice(indexOfUser, 1);
-  }
-  return res.json(user);
+server.delete("/user/:id", checkIndexFormat, (req, res) => {
+  const { id } = req.params;
+  users.splice(id, 1);
+  return res.send();
 });
 
 server.listen(3010);
